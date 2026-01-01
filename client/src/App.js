e10b7e1d07e44470bc4a9d4b34d518548d87489a
Soumik seth <soumikseth4@gmail.com>
2026-01-01 12:05:31 +0530

    I have add admin route in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import Contact from "./pages/Contact";
 import Register from "./pages/Register";
 import ShowAllBooks from "./pages/ShowAllBooks";
+import AdminDashboard from "./pages/AdminDashboard";
 function App() {
   return (
     <Router>
@@ -17,6 +18,7 @@ function App() {
         <Route path="/register" element={<Register />} />
       <Route path="/books" element={<ShowAllBooks />} />
       <Route path="/Contact" element={<Contact />} />
+      <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
