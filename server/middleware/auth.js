c5f3264e43fd4c95b55b610546c43ef9d979e43c
Soumik seth <soumikseth4@gmail.com>
2026-01-10 11:39:31 +0530

    all are ok

---
+++ server/middleware/auth.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+const JWT = process.env.JWT_SECRET;
+module.exports=(req,res,next)=>{
+    const token =req.header.authorization?.split("")[1];
+    if(!token){
+        return res.status(401).json({msg:"No token"});
+    }
+    req.user= jwt.verify(token,JWT);
+    next();
+}
---
+++ server/routes/books.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const Book =require('../models/Book');
+const auth=require('../middleware/auth');
+
+//get all books
+router.get('/',auth,async(req,res)=>{
+    res.json(await Book.find());
+});
+
+//borrow a book
+router.post('/borrow/:id',auth,async(req,res)=>{
+    await Book.findByIdAndUpdate(req.params.id,{
+        ststus:borrowed,
+        borrowedBy:req.user.id
+    });
+    res.json({message:'book borrowed successfully'});
+
+})
+
+////return a book
+router.post('return/:id',auth,async(req,res)=>{
+    await Book.findByIdAndUpdate(req.params.id,{
+        ststus:available,
+        borrowedBy:""
+    });
+    res.json({message:'book returned successfully'});
+});
+
+module.exports=router;
