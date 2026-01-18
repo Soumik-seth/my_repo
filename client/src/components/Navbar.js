b1c66f11959686ed6097f5dccf44eb3b4d5fc318
Soumik seth <soumikseth4@gmail.com>
2026-01-18 11:40:00 +0530

    logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import {logo} from "../assets/library_log.png";
+import logo from "../assets/library_log.png";
 
 function Navbar() {
   const [role, setRole] = useState(null);
