6f7c56b87acd6c5d02e76925a2171f14681d88f6
Soumik seth <soumikseth4@gmail.com>
2026-02-05 20:26:13 +0530

    delete

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,9 +10,7 @@ function Footer (){
          <li>
       <Link to="/">HOME</Link>
          </li>
-         <li>
-      <Link to="/about">ABOUT</Link>
-         </li>
+
          <li>
       <Link to="/contact">CONTACT</Link>
          </li>
