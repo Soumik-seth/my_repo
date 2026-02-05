ef3020e295874cfefbeaabb27f8b1efae3a6fb36
Soumik seth <soumikseth4@gmail.com>
2026-02-05 20:40:41 +0530

    ko

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../src/components/Navbar.css';
-import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
